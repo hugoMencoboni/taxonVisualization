cd9f249fe97e1700cebad89bc94513ddbd2f3860
hugo.M <hugo.mencoboni@gmail.com>
2020-04-13 17:14:58 +0200

    Feature: création du formulaire de test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormTestComponent } from './form-test/form-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-test/form-test.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { pairwise, startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'base-form-test',
+  templateUrl: './form-test.component.html',
+  styleUrls: ['./form-test.component.css']
+})
+export class FormTestComponent implements OnInit, OnDestroy {
+
+  form: FormGroup;
+  subscription = new Subscription();
+  lastChange: { formName: string, value: string };
+  get lastChangeFormated(): string {
+    return this.lastChange ? `${this.lastChange.formName} : ${this.lastChange.value}` : '';
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      input: ['', Validators.required]
+    });
+
+    this.subscription.add(
+      this.form.valueChanges.pipe(startWith(this.form.value), pairwise()).subscribe(([oldvalues, newValues]) => {
+        let keyChanged: string;
+        let valueChanged: string;
+
+        Object.keys(newValues).forEach(property => {
+          if (newValues[property] !== oldvalues[property]) {
+            keyChanged = property;
+            valueChanged = newValues[property];
+          }
+        });
+
+        this.lastChange = {
+          formName: keyChanged,
+          value: valueChanged
+        };
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  formSend(event): void {
+    const a = event;
+  }
+}
