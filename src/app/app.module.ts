f743a16f6068a2af68c97e10afcf676e9fc57cec
hugo.M <hugo.mencoboni@gmail.com>
2020-05-01 21:06:07 +0200

    Feature: netoyage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,17 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormTestComponent } from './form-test/form-test.component';
-import { AutocompleteModule } from './shared/form-base/autocomplete-base/autocomplete-base.component';
-import { DateModule } from './shared/form-base/date-base/date-base.component';
-import { InputModule } from './shared/form-base/input-base/input-base.component';
-import { SelectModule } from './shared/form-base/select-base/select-base.component';
 import { SharedModule } from './shared/shared.module';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormTestComponent
+    TreeComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    InputModule,
-    AutocompleteModule,
-    DateModule,
-    SelectModule,
     SharedModule,
   ],
   providers: [],
---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree',
+  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.scss']
+})
+export class TreeComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+}
