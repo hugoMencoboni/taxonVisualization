6ba0f863bf9ba341b063c3257d2c26e282c5ac54
hugo.M <hugo.mencoboni@gmail.com>
2020-05-25 19:52:03 +0200

    Feature: déplacement du SVG

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+import { interval, Subject, Subscription } from 'rxjs';
+import { map, pairwise, take } from 'rxjs/operators';
 import { DataItem, GetTreeItem, TreeItem } from '../core/models/tree/item.model';
 import { ColorService } from '../core/services/color.service';
 import { DataService } from '../core/services/data.service';
@@ -10,11 +11,21 @@ import { DataService } from '../core/services/data.service';
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss']
 })
-export class TreeComponent implements OnInit, OnDestroy {
+export class TreeComponent implements OnInit, AfterViewInit, OnDestroy {
   subscription = new Subscription();
 
-  focusPositionX = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
-  focusPositionY = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) / 2;
+  @ViewChild('tree')
+  public svg: ElementRef;
+  svgWidth: number;
+  svgHeight: number;
+  svgInitPositionX: number;
+  svgInitPositionY: number;
+  svgPositionX: number;
+  svgPositionY: number;
+  svgPosition$ = new Subject<{ x: number, y: number }>();
+
+  focusPositionX: number;
+  focusPositionY: number;
 
   distanceX = 350;
   distanceY = 120;
@@ -23,32 +34,64 @@ export class TreeComponent implements OnInit, OnDestroy {
 
   datas: Array<TreeItem>;
 
-  constructor(private dataService: DataService, private colorService: ColorService) { }
+  onMove = false;
+
+  constructor(private dataService: DataService, private colorService: ColorService) {
+    const svgWidth = 10000;
+    this.svgWidth = svgWidth;
+    this.svgHeight = svgWidth;
+    this.svgPositionX = (-1) * svgWidth / 2;
+    this.svgPositionY = (-1) * svgWidth / 2;
+    this.svgInitPositionX = this.svgPositionX;
+    this.svgInitPositionY = this.svgPositionY;
+    this.focusPositionX = svgWidth / 2 + (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
+    this.focusPositionY = svgWidth / 2 + (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) / 2;
+  }
 
   ngOnInit() {
     this.subscription = this.dataService.activeSeed$().subscribe((seed: DataItem) => {
       this.datas = [GetTreeItem(seed, this.focusPositionX, this.focusPositionY, null)];
     });
+
+    this.subscription.add(this.svgPosition$.asObservable().pipe(pairwise()).subscribe(([previousPos, newPos]) => {
+      if (previousPos) {
+        const diffX = newPos.x - previousPos.x;
+        const diffY = newPos.y - previousPos.y;
+        if (Math.abs(diffX) + Math.abs(diffY) < 100) {
+          const force = 1.5;
+          this.svgPositionX += diffX * force;
+          this.svgPositionY += diffY * force;
+        }
+      }
+    }));
+  }
+
+  ngAfterViewInit(): void {
+    const svg = this.svg.nativeElement;
+
+    d3.select(svg)
+      .on('mousedown', () => {
+        if (d3.event.target.id === 'tree') {
+          this.onMove = true;
+        }
+      })
+      .on('mouseup', () => this.onMove = false)
+      .on('mouseleave', () => this.onMove = false)
+      .on('mouseout', () => {
+        if (!d3.event.relatedTarget) {
+          this.onMove = false;
+        }
+      });
+
+    window.addEventListener('mousemove', (evt) => {
+      if (this.onMove) {
+        this.svgPosition$.next({ x: evt.screenX, y: evt.screenY });
+      }
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-  }
-
-  moveRigth(): void {
-    this.datas.forEach(d => d.x -= 500);
-  }
-
-  moveLeft(): void {
-    this.datas.forEach(d => d.x += 500);
-  }
-
-  moveUp(): void {
-    this.datas.forEach(d => d.y += 300);
-  }
-
-  moveDown(): void {
-    this.datas.forEach(d => d.y -= 300);
   }
 
   getItem(id: number): TreeItem {
@@ -62,6 +105,8 @@ export class TreeComponent implements OnInit, OnDestroy {
 
   onItemSelected(id: number): void {
     const item = this.getItem(id);
+
+    this.resetSVGPosition();
 
     this.dataService.changeActiveItem(id);
 
@@ -175,5 +220,36 @@ export class TreeComponent implements OnInit, OnDestroy {
       x: parent.x + this.distanceX,
       y
     };
+  }
+
+  private resetSVGPosition() {
+    if (this.svgPositionX !== this.svgInitPositionX || this.svgPositionY !== this.svgInitPositionY) {
+      const transitionTime = 500; // en ms
+      const diffX = Math.abs(this.svgInitPositionX - this.svgPositionX);
+      const diffY = Math.abs(this.svgInitPositionY - this.svgPositionY);
+      const transitionPositions = new Array<{ x: number, y: number }>();
+      const diffMax = Math.max(diffX, diffY);
+      let prevX = this.svgPositionX;
+      let prevY = this.svgPositionY;
+      for (let index = 0; index < diffMax; index++) {
+        const x = this.svgPositionX + Math.floor(((index + 1) / diffMax) * (this.svgInitPositionX - this.svgPositionX));
+        const y = this.svgPositionY + Math.floor(((index + 1) / diffMax) * (this.svgInitPositionY - this.svgPositionY));
+        if (Math.abs(prevX - x) + Math.abs(prevY - y) > 20) {
+          transitionPositions.push({ x, y });
+          prevX = x;
+          prevY = y;
+        }
+      }
+
+      interval(transitionTime / transitionPositions.length)
+        .pipe(
+          take(transitionPositions.length),
+          map(i => transitionPositions[i])
+        )
+        .subscribe(newPosition => {
+          this.svgPositionX = newPosition.x;
+          this.svgPositionY = newPosition.y;
+        });
+    }
   }
 }
